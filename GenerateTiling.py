a9c3ae71aab3f38726bdf8c458d07a36acfc8c98
r0ckwav3 <peterrockwave@gmail.com>
2022-04-08 20:02:19 -0700

    Split some of trueMovePos into legalMove, bugs

--- GenerateTiling.py
+++ GenerateTiling.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 SIZE = 4
@@ -35,9 +36,9 @@ def randomMove():
 
 
 # given a move, return the coordinates of the pile it affects
-# if the move is not legal, returns None
+# if there are no possible coordinates, returns None
 def trueMovePos(heights, move):
-    temppos = move[0][:]  # make a copy
+    temppos = copy.deepcopy(move[0])  # make a copy
     while max(temppos) < SIZE:
         targetheight = temppos[2]
         if move[1]:
@@ -53,10 +54,32 @@ def trueMovePos(heights, move):
     return None
 
 
+# given a move, return the coordinates of the pile it affects
+# if the move is illegal, returns None
+def legalMove(heights, move):
+    pos = trueMovePos(heights, move)
+    if pos:
+        if move[1]:
+            if pos[0] != SIZE-1:
+                if heights[pos[0]][pos[1]] <= heights[pos[0]+1][pos[1]]:
+                    return None
+            if pos[1] != SIZE-1:
+                if heights[pos[0]][pos[1]] <= heights[pos[0]][pos[1]+1]:
+                    return None
+        else:
+            if pos[0] != 0:
+                if heights[pos[0]][pos[1]] >= heights[pos[0]-1][pos[1]]:
+                    return None
+            if pos[1] != 0:
+                if heights[pos[0]][pos[1]] >= heights[pos[0]][pos[1]-1]:
+                    return None
+    return pos
+
+
 # destructive, modifies the configuration passed in
 # does nothing if the move is illegal
 def performMove(heights, move):
-    movepos = trueMovePos(heights, move)
+    movepos = legalMove(heights, move)
     if movepos is not None:
         if move[1]:
             heights[movepos[0]][movepos[1]] -= 1
@@ -66,7 +89,7 @@ def performMove(heights, move):
 
 # nondestructive, performs moves back to front
 def performMoves(heights, move_list):
-    newheights = heights[:]  # copy it over
+    newheights = copy.deepcopy(heights)
     for i in range(len(move_list)-1, -1, -1):
         performMove(newheights, move_list[i])
     return newheights
